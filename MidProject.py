befa8b080807c02c2be1e7e68571fc5aee8bc34f
chihyuns0ng <72368966+chihyuns0ng@users.noreply.github.com>
2022-10-20 18:35:13 +0900

    Add files via upload

--- MidProject.py
+++ MidProject.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import koreanize_matplotlib
+# import plotly.graph_objects as go
 
 st.set_page_config(
     page_title="1론머스크 MidProject",
@@ -14,6 +15,8 @@ st.set_page_config(
 
 st.markdown("# 🚀1론머스크🚀")
 st.sidebar.markdown("# 공공데이터")
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 url = ["https://raw.githubusercontent.com/chihyuns0ng/AIS7MidProject/main/data/kosis3_19.csv"
 , "https://raw.githubusercontent.com/chihyuns0ng/AIS7MidProject/main/data/kosis3_20.csv"
@@ -40,11 +43,89 @@ kosis3_20_number = kosis3_20_number.rename_axis("가구원수")
 kosis3_21_number = kosis3_21_number.set_index("특성별(2)")
 kosis3_21_number = kosis3_21_number.rename_axis("가구원수")
 
-fig, ax = plt.subplots()
-# kosis3_19_number.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
-sns.barplot(data=kosis3_19_number.T[:-2])
+
+# a = kosis3_19_number.T[:-2]
+# a["index"] = kosis3_19_number.T[:-2].index
+# data1 = go.Bar(x=a["index"], y=a["1인"], name="1인")
+# data2 = go.Bar(x=a["index"], y=a["2인"], name="2인")
+# data3 = go.Bar(x=a["index"], y=a["3인"], name="3인")
+# data4 = go.Bar(x=a["index"], y=a["4인"], name="4인")
+# data5 = go.Bar(x=a["index"], y=a["5인 이상"], name="5인 이상")
+# layout = go.Layout(title="2019년 가구원수별")
+# fig = go.Figure(data=[data1, data2, data3, data4, data5], layout=layout)
+# fig.show()
+# sns.barplot(data=kosis3_19_number.T[:-2])
+# st.bar_chart(kosis3_19_number.T[["1인", "2인", "3인", "4인", "5인 이상"]])
+
+# pxh = px.histogram(kosis3_19_number)
+# go.Bar()
+
+
+fig = kosis3_19_number.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
 plt.title("2019년 가구원수별", fontsize=20)
 plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
-st.pyplot(fig)
+st.pyplot(plt.show())
 
-# st.dataframe(kosis3_19_number)
+fig = kosis3_20_number.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
+plt.title("2019년 가구원수별", fontsize=20)
+plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
+st.pyplot(plt.show())
+
+fig = kosis3_21_number.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
+plt.title("2019년 가구원수별", fontsize=20)
+plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
+st.pyplot(plt.show())
+
+
+kosis3_19_gender = kosis3_19[6:8].drop("특성별(1)", axis=1)
+kosis3_20_gender = kosis3_20[6:8].drop("특성별(1)", axis=1)
+kosis3_21_gender = kosis3_21[5:7].drop("특성별(1)", axis=1)
+
+kosis3_19_gender = kosis3_19_gender.set_index("특성별(2)")
+kosis3_19_gender = kosis3_19_gender.rename_axis("가구원수")
+kosis3_20_gender = kosis3_20_gender.set_index("특성별(2)")
+kosis3_20_gender = kosis3_20_gender.rename_axis("가구원수")
+kosis3_21_gender = kosis3_21_gender.set_index("특성별(2)")
+kosis3_21_gender = kosis3_21_gender.rename_axis("가구원수")
+
+fig = kosis3_19_gender.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
+plt.title("2019년 가구원수별", fontsize=20)
+plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
+st.pyplot(plt.show())
+
+fig = kosis3_20_gender.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
+plt.title("2019년 가구원수별", fontsize=20)
+plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
+st.pyplot(plt.show())
+
+fig = kosis3_21_gender.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
+plt.title("2019년 가구원수별", fontsize=20)
+plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
+st.pyplot(plt.show())
+
+
+kosis3_19_age = kosis3_19[8:].drop("특성별(1)", axis=1)
+kosis3_20_age = kosis3_20[8:].drop("특성별(1)", axis=1)
+kosis3_21_age = kosis3_21[7:].drop("특성별(1)", axis=1)
+
+kosis3_19_age = kosis3_19_age.set_index("특성별(2)")
+kosis3_19_age = kosis3_19_age.rename_axis("가구원수")
+kosis3_20_age = kosis3_20_age.set_index("특성별(2)")
+kosis3_20_age = kosis3_20_age.rename_axis("가구원수")
+kosis3_21_age = kosis3_21_age.set_index("특성별(2)")
+kosis3_21_age = kosis3_21_age.rename_axis("가구원수")
+
+fig = kosis3_19_age.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
+plt.title("2019년 가구원수별", fontsize=20)
+plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
+st.pyplot(plt.show())
+
+fig = kosis3_20_age.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
+plt.title("2019년 가구원수별", fontsize=20)
+plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
+st.pyplot(plt.show())
+
+fig = kosis3_21_age.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
+plt.title("2019년 가구원수별", fontsize=20)
+plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
+st.pyplot(plt.show())
