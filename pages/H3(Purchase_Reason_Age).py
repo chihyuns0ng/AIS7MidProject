eb04290cee5b9fc1d89b6d1a2fc585879c5efb23
chihyuns0ng <72368966+chihyuns0ng@users.noreply.github.com>
2022-10-21 00:58:37 +0900

    Update H3(Purchase_Reason_Age).py

--- pages/H3(Purchase_Reason_Age).py
+++ pages/H3(Purchase_Reason_Age).py
@@ -40,15 +40,27 @@ kosis3_20_age = kosis3_20_age.rename_axis("가구원수")
 kosis3_21_age = kosis3_21_age.set_index("특성별(2)")
 kosis3_21_age = kosis3_21_age.rename_axis("가구원수")
 
+st.write("""
+### 2019년
+""")
+
 fig = kosis3_19_age.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
 plt.title("2019년 연령별", fontsize=20)
 plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
 st.pyplot(plt.show())
 
+st.write("""
+### 2020년
+""")
+
 fig = kosis3_20_age.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
 plt.title("2020년 연령별", fontsize=20)
 plt.legend(fontsize=20, bbox_to_anchor=(1.2,1))
 st.pyplot(plt.show())
+
+st.write("""
+### 2021년
+""")
 
 fig = kosis3_21_age.T[:-2].plot(kind="bar", figsize=(20,10),fontsize=15, rot=20)
 plt.title("2021년 연령별", fontsize=20)
