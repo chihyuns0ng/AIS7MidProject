bd719e986cd77caba8be06aa3986d45df7b51ace
chihyuns0ng <72368966+chihyuns0ng@users.noreply.github.com>
2022-10-21 02:34:41 +0900

    Update C(Purchase_Change).py

--- pages/C(Purchase_Change).py
+++ pages/C(Purchase_Change).py
@@ -16,67 +16,58 @@ st.markdown("# 간편식 구입변화📈")
 st.sidebar.markdown("# 간편식 구입변화📈")
 
 
-ur1="https://raw.githubusercontent.com/chihyuns0ng/AIS7MidProject/main/data/%E1%84%80%E1%85%A1%E1%86%AB%E1%84%91%E1%85%A7%E1%86%AB%E1%84%89%E1%85%B5%E1%86%A8_%E1%84%91%E1%85%AE%E1%86%B7%E1%84%86%E1%85%A9%E1%86%A8%E1%84%87%E1%85%A7%E1%86%AF_%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%83%E1%85%A9%E1%84%87%E1%85%A7%E1%86%AF_%E1%84%80%E1%85%AE%E1%84%8B%E1%85%B5%E1%86%B8%E1%84%87%E1%85%A7%E1%86%AB%E1%84%92%E1%85%AA.csv"
-df_ch = pd.read_csv(ur1)
+url_7 = "https://raw.githubusercontent.com/hj2628/AIS7MidProject/main/%E1%84%80%E1%85%A1%E1%86%AB%E1%84%91%E1%85%A7%E1%86%AB%E1%84%89%E1%85%B5%E1%86%A8_%E1%84%91%E1%85%AE%E1%86%B7%E1%84%86%E1%85%A9%E1%86%A8%E1%84%87%E1%85%A7%E1%86%AF_%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%83%E1%85%A9%E1%84%87%E1%85%A7%E1%86%AF_%E1%84%80%E1%85%AE%E1%84%8B%E1%85%B5%E1%86%B8%E1%84%87%E1%85%A7%E1%86%AB%E1%84%92%E1%85%AA"
 
-df_ch = df_ch.astype({'연도' : 'int'})
-df_ch = df_ch.astype({'매우감소' : 'float'})
-df_ch = df_ch.astype({'약간감소' : 'float'})
-df_ch = df_ch.astype({'비슷' : 'float'})
-df_ch = df_ch.astype({'약간증가' : 'float'})
-df_ch = df_ch.astype({'매우증가' : 'float'})
-df_ch = df_ch.astype({'평균' : 'float'})
+@st.cache
+def load_data(url_7):
+    data_7 = pd.read_csv(url_7)
+    return data_7
+
+data_7 = load_data(url_7)
+
+st.dataframe(data_7)
 
 st.write("""
 ### 매우 감소
 """)
 
-fig, ax = plt.subplots()
-sns.barplot(data=df_ch, x="연도", y="매우감소", hue="품목군").set_title("간편식 구입변화(%)")
-plt.legend(bbox_to_anchor=(1,1))
-st.pyplot(fig)
+plt.figure(figsize=(15, 5))
+sns.barplot(data=data_7, x="연도", y="매우감소", hue="품목군").set_title("연도별 간편식 구입변화(%)")
+plt.legend(bbox_to_anchor=(1.12,1))
+st.pyplot()
 
 st.write("""
 ### 약간 감소
 """)
 
-fig, ax = plt.subplots()
-sns.barplot(data=df_ch, x="연도", y="약간감소", hue="품목군").set_title("간편식 구입변화(%)")
-plt.legend(bbox_to_anchor=(1,1))
-st.pyplot(fig)
+plt.figure(figsize=(15, 5))
+sns.barplot(data=data_7, x="연도", y="약간감소", hue="품목군").set_title("연도별 간편식 구입변화(%)")
+plt.legend(bbox_to_anchor=(1.12,1))
+st.pyplot()
 
 st.write("""
 ### 비슷
 """)
 
-fig, ax = plt.subplots()
-sns.barplot(data=df_ch, x="연도", y="비슷", hue="품목군").set_title("간편식 구입변화")
-plt.legend(bbox_to_anchor=(1,1))
-st.pyplot(fig)
+plt.figure(figsize=(15, 5))
+sns.barplot(data=data_7, x="연도", y="비슷", hue="품목군").set_title("연도별 간편식 구입변화(%)")
+plt.legend(bbox_to_anchor=(1.12,1))
+st.pyplot()
 
 st.write("""
 ### 약간 증가
 """)
 
-fig, ax = plt.subplots()
-sns.barplot(data=df_ch, x="연도", y="약간증가", hue="품목군").set_title("간편식 구입변화(%)")
-plt.legend(bbox_to_anchor=(1,1))
-st.pyplot(fig)
+plt.figure(figsize=(15, 5))
+sns.barplot(data=data_7, x="연도", y="약간증가", hue="품목군").set_title("연도별 간편식 구입변화(%)")
+plt.legend(bbox_to_anchor=(1.12,1))
+st.pyplot()
 
 st.write("""
 ### 매우 증가
 """)
 
-fig, ax = plt.subplots()
-sns.barplot(data=df_ch, x="연도", y="매우증가", hue="품목군").set_title("간편식 구입변화(%)")
-plt.legend(bbox_to_anchor=(1,1))
-st.pyplot(fig)
-
-st.write("""
-### 평균
-""")
-
-fig, ax = plt.subplots()
-sns.barplot(data=df_ch, x="연도", y="평균", hue="품목군").set_title("간편식 구입변화 평균(점)")
-plt.legend(bbox_to_anchor=(1,1))
-st.pyplot(fig)
+plt.figure(figsize=(15, 5))
+sns.barplot(data=data_7, x="연도", y="매우증가", hue="품목군").set_title("연도별 간편식 구입변화(%)")
+plt.legend(bbox_to_anchor=(1.12,1))
+st.pyplot()
