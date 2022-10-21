d08ccb03d9ce36c767ac90b434755545ffbb6c0f
chihyuns0ng <72368966+chihyuns0ng@users.noreply.github.com>
2022-10-21 14:29:43 +0900

    Update B(Purchase_Data).py

--- pages/B(Purchase_Data).py
+++ pages/B(Purchase_Data).py
@@ -15,6 +15,8 @@ url = "https://raw.githubusercontent.com/hj2628/AIS7MidProject/main/2019_%E1%84%
 
 url_2 = "https://raw.githubusercontent.com/hj2628/AIS7MidProject/main/2020_%E1%84%80%E1%85%AE%E1%84%86%E1%85%A2%E1%84%83%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%90%E1%85%A5_%E1%84%8B%E1%85%AF%E1%86%AF%E1%84%87%E1%85%A7%E1%86%AF_%E1%84%91%E1%85%AE%E1%86%B7%E1%84%86%E1%85%A9%E1%86%A8%E1%84%80%E1%85%AE%E1%86%AB%E1%84%87%E1%85%A7%E1%86%AF_%E1%84%80%E1%85%AE%E1%84%86%E1%85%A2%E1%84%8E%E1%85%AE%E1%84%8B%E1%85%B5"
 
+url_3 = "https://raw.githubusercontent.com/chihyuns0ng/AIS7MidProject/main/data/purchase_data.csv
+
 
 @st.cache
 def load_data(url):
@@ -26,16 +28,22 @@ def load_data(url_2):
     data_2 = pd.read_csv(url_2)
     return data_2
 
+@st.cache
+def load_data(url_3):
+    data_3 = pd.read_csv(url_3)
+    return data_3
+
 
 data = load_data(url)
 data_2 = load_data(url_2)
+data_3 = load_data(url_3)
 
 st.markdown("## 🚀19~20년도 구매데이터🚀")
 st.sidebar.markdown("# 🚀19~20년도 구매데이터🚀")
 
 st.dataframe(data)
 st.dataframe(data_2)
-
+st.dataframe(data_3)
 
 pxh = px.histogram(data, x="월", y="Sales_Unit", color="MasterCategoryFullName", histfunc="sum", barmode="group", title="19년 품목별 구매추이")
 st.plotly_chart(pxh)
@@ -43,3 +51,15 @@ st.plotly_chart(pxh)
 
 pxh = px.histogram(data_2, x="월", y="Sales_Unit", color="MasterCategoryFullName", histfunc="sum", barmode="group", title="20년 품목별 구매추이")
 st.plotly_chart(pxh)
+
+
+data_3 = data3[["count"]]
+data_3.loc["Others"] = data_3["count"][5:].sum()
+
+data_3 = data_3.drop(["생수 · 과자 · 라면 · 커피", "생선 · 건해산물", "주류", "치킨 · 초밥 · 베이커리"
+                  , "견과 · 선식 · 차류", "쌀 · 잡곡", "장 건강", "홍삼 · 면역", "비타민 · 미네랄", "건강소재 · 꿀"])
+
+
+fig = plt.pie(df_3["count"], labels=df_3.index, startangle=180, autopct='%1.1f%%', counterclock=False, wedgeprops=dict(width=0.5))
+plt.legend(bbox_to_anchor=(2,1))
+st.pyplot()
