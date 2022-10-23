cc4d8e60c89e25971daa6e415dff092ff6ab5476
HyenC <38906574+HyenC@users.noreply.github.com>
2022-10-23 20:08:34 +0900

    Update D(Trend).py

--- pages/D(Trend).py
+++ pages/D(Trend).py
@@ -45,7 +45,6 @@ df_21 = load_data(url21)
 df_t19 = df_19.rename(columns=df_19.iloc[0])
 df_t19 = df_t19.drop(df_t19.index[0])
 df_t19
-df_t19 = df_t19.astype({i:"float" for i in df_t19.columns[2:]})
 
 st.write("""
 ### 2020년
@@ -53,7 +52,6 @@ st.write("""
 df_t20 = df_20.rename(columns=df_20.iloc[0])
 df_t20 = df_t20.drop(df_t20.index[0])
 df_t20
-df_t20 = df_t20.astype({i:"float" for i in df_t20.columns[2:]})
 
 st.write("""
 ### 2021년
@@ -61,52 +59,3 @@ st.write("""
 df_t21 = df_21.rename(columns=df_21.iloc[0])
 df_t21 = df_t21.drop(df_t21.index[0])
 df_t21
-df_t21 = df_t21.astype({i:"float" for i in df_t21.columns[2:]})
-
-
-df19_c = df_t19[df_t19['특성별(1)'] == '가구원수별']
-df_19_c = df19_c.drop(['특성별(1)'], axis=1)
-df_19_c = df_19_c.set_index(keys='특성별(2)')
-df_19_c = df_19_c.rename_axis('가구원수별')
-
-df19_s = df_t19[df_t19['특성별(1)'] == '가구주성별']
-df_19_s = df19_s.drop(['특성별(1)'], axis=1)
-df_19_s = df_19_s.set_index(keys='특성별(2)')
-df_19_s = df_19_s.rename_axis('성별')
-
-df19_a = df_t19[df_t19['특성별(1)'] == '가구주연령별']
-df_19_a = df19_a.drop(['특성별(1)'], axis=1)
-df_19_a = df_19_a.set_index(keys='특성별(2)')
-df_19_a = df_19_a.rename_axis('연령별')
-
-
-df20_c = df_t20[df_t20['특성별(1)'] == '가구원수별']
-df_20_c = df20_c.drop(['특성별(1)'], axis=1)
-df_20_c = df_20_c.set_index(keys='특성별(2)')
-df_20_c = df_20_c.rename_axis('가구원수별')
-
-df20_s = df_t20[df_t20['특성별(1)'] == '가구주성별']
-df_20_s = df20_s.drop(['특성별(1)'], axis=1)
-df_20_s = df_20_s.set_index(keys='특성별(2)')
-df_20_s = df_20_s.rename_axis('성별')
-
-df20_a = df_t20[df_t20['특성별(1)'] == '가구주연령별']
-df_20_a = df20_a.drop(['특성별(1)'], axis=1)
-df_20_a = df_20_a.set_index(keys='특성별(2)')
-df_20_a = df_20_a.rename_axis('연령별')
-
-
-df21_c = df_t21[df_t21['특성별(1)'] == '가구원수별']
-df_21_c = df21_c.drop(['특성별(1)'], axis=1)
-df_21_c = df_21_c.set_index(keys='특성별(2)')
-df_21_c = df_21_c.rename_axis('가구원수별')
-
-df21_s = df_t21[df_t21['특성별(1)'] == '가구주성별']
-df_21_s = df21_s.drop(['특성별(1)'], axis=1)
-df_21_s = df_21_s.set_index(keys='특성별(2)')
-df_21_s = df_21_s.rename_axis('성별')
-
-df21_a = df_t21[df_t21['특성별(1)'] == '가구주연령별']
-df_21_a = df21_a.drop(['특성별(1)'], axis=1)
-df_21_a = df_21_a.set_index(keys='특성별(2)')
-df_21_a = df_21_a.rename_axis('연령별')
