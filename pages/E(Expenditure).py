089e9bae403c43b15978bc520748af90381ae63f
HyenC <38906574+HyenC@users.noreply.github.com>
2022-10-24 13:52:26 +0900

    Update E(Expenditure).py

--- pages/E(Expenditure).py
+++ pages/E(Expenditure).py
@@ -7,8 +7,8 @@ import plotly.express as px
 import koreanize_matplotlib
 
 st.set_page_config(
-    page_title="평균 지출액",
-    page_icon="💲",
+    page_title="🚀일론머스크 MID🚀",
+    page_icon="⭐️",
     layout="wide",
 )
 
